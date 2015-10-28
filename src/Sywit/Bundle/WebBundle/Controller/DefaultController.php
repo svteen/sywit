b1737a122d20da6299fa57ea39e0e6b75f6ea3e8
svteen <524832917@qq.com>
2015-10-29 01:02:59 +0800

    remove

--- src/Sywit/Bundle/WebBundle/Controller/DefaultController.php
+++ src/Sywit/Bundle/WebBundle/Controller/DefaultController.php
@@ -4,7 +4,7 @@ namespace Sywit\Bundle\WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+//use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class DefaultController extends Controller
 {
@@ -16,8 +16,8 @@ class DefaultController extends Controller
         //return array('name' => $name);
         $name = "Default";
 
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => $name
+        return $this->render('SywitWebBundle:Default:index.html.twig', array(
+            'name' => $name
         ));
     }
 }
